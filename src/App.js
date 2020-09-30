190ec3a27e086d65e259a18ee89f665feb15c841
Dmytro Myronenko <dim@uain.su>
2020-09-30 12:56:15 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React from 'react';
 import TodoList from "./Todo/TodoList";
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'To buy 1'},
-    {id: 2, completed: false, title: 'To buy 2'},
+    {id: 2, completed: true, title: 'To buy 2'},
     {id: 3, completed: false, title: 'To buy 3'}
-  ]
+  ])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    )
+  }
 
   return (
     <div className='wrapper'>
       <h1>React tutorial</h1>
 
-      <TodoList todos={todos}></TodoList>
+      <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
   );
 }
